700bd70aa591f0d3483b6693d7f23021efc2a617
StefanGrimminck <git@stefangrimminck.nl>
2025-12-01 16:52:32 +0100

    chore(e2e): set GOFLAGS=-buildvcs=false in container run to avoid VCS stamping failures

--- scripts/run_e2e_in_container.sh
+++ scripts/run_e2e_in_container.sh
@@ -10,5 +10,6 @@ docker build -t "$IMAGE_NAME" -f Dockerfile .
 # We add NET_ADMIN and NET_RAW so the container can manipulate iptables inside its own namespace.
 docker run --rm -it \
   --cap-add=NET_ADMIN --cap-add=NET_RAW \
+  -e GOFLAGS=-buildvcs=false \
   -v "$(pwd)":/work -w /work \
   "$IMAGE_NAME" bash -lc "scripts/run_e2e_tests.sh"
